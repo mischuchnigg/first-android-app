837cfd4c292d29a051f6728c822eaf23520a1732
Michael Schuchnigg <mischuchnigg@edu.aau.at>
2023-03-12 18:33:57 +0100

    Added all functions

--- app/src/main/java/com/example/networktest/CLientSocket.java
+++ app/src/main/java/com/example/networktest/CLientSocket.java
@@ -1,2 +1,61 @@
-package com.example.networktest;public class CLientSocket {
+package com.example.networktest;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class CLientSocket extends Thread {
+    private String hostname;
+    private int port;
+    private String sendMsg;
+    Socket socket;
+    DataOutputStream outToServer;
+    BufferedReader r;
+
+    public String getSendMsg() {
+        return sendMsg;
+    }
+
+    public void setSendMsg(String sendMsg) {
+        this.sendMsg = sendMsg;
+    }
+
+    public String getRecieveMsg() {
+        return recieveMsg;
+    }
+
+    public void setRecieveMsg(String recieveMsg) {
+        this.recieveMsg = recieveMsg;
+    }
+
+    private String recieveMsg;
+    public CLientSocket(String hostname, int port){
+        this.port = port;
+        this.hostname = hostname;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+                socket = new Socket(this.hostname, this.port);
+                outToServer = new DataOutputStream(socket.getOutputStream());
+                r = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            outToServer.writeBytes(sendMsg + '\n');
+            this.recieveMsg = r.readLine();
+            //MainActivity.setTextField(recieveMsg);
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -7,37 +7,65 @@ import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
 
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     String hostname = "se2-isys.aau.at";
     int port = 53212;
-    Socket sock;
-    Button btn;
+    CLientSocket sock;
+    Button btn1;
+    Button btCalc;
     EditText nrInput;
+
+    static TextView outText;
     String mrNr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* try {
-            sock = new Socket(hostname,port);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }*/
-        nrInput = findViewById(R.id.editTextNumber);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mrNr = nrInput.getText().toString();
-                Log.d("TAG",mrNr);
 
+        nrInput = findViewById(R.id.editTextNumber);
+        outText = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(view -> {
+            mrNr = nrInput.getText().toString();
+            sock = new CLientSocket(hostname,port);
+            sock.setSendMsg(mrNr);
+            sock.start();
+            while(sock.getRecieveMsg()==null){
             }
+            outText.setText(sock.getRecieveMsg());
         });
+        btCalc = findViewById(R.id.button2);
+        btCalc.setOnClickListener(view -> {
+            mrNr = nrInput.getText().toString();
+            int sum = 0;
+            for(int i = 0;i<mrNr.length();i++){
+                int number = Integer.parseInt(String.valueOf(mrNr.charAt(i)));
+                if(i%2==1){
+                    sum = sum - number;
+                }else sum = sum + number;
+            }
+            if(sum%2==0)
+            outText.setText("Die Alternierende Quersumme ist gerade");
+            else outText.setText("Die Alternierende Quersumme ist ungerade");
+
+
+        });
+
+
+    }
+    public static void setTextField(String msg){
+        outText.setText(msg);
+
     }
 }
